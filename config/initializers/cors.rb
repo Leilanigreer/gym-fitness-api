cb29e6b4682ecd58588d5d953aed8cad60a92310
Leilanigreer <leilani@lpcgolf.com>
2024-11-14 11:15:04 -0800

    updated cors for Railway

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173", "http://localhost:3000", "https://gym-fitness-frontend-production.up.railway.app"
+    origins "http://localhost:5173",
+            "http://localhost:3000",
+            "https://gym-fitness-frontend-production.up.railway.app",
+            "https://gym-fitness-api.railway.internal"
 
     resource "*",
       headers: :any,

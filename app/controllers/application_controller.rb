6db439d23aec6c996ae08ae431e3a9385841c445
Leilanigreer <leilani@lpcgolf.com>
2024-11-15 10:56:28 -0800

    added .downcase to email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  allow_browser versions: :modern
   protect_from_forgery with: :exception, unless: -> { request.format.json? }
 
   def current_user

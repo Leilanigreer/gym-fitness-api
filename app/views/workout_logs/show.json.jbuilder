b15ef15f0f1a8778b1e5924d2190013c3c4d740d
Leilanigreer <leilani@lpcgolf.com>
2024-11-19 10:07:19 -0800

    added tailwind frontend to cors

--- app/views/workout_logs/show.json.jbuilder
+++ app/views/workout_logs/show.json.jbuilder
@@ -5,7 +5,6 @@ json.workout_log do
     json.id @workout_log.routine.id
     json.exercise_id @workout_log.routine.exercise_id
     json.exercise_name @workout_log.routine.exercise.name
-    json.exercise @workout_log.exercise
     json.reps @workout_log.routine.reps
     json.sets @workout_log.routine.sets
     json.day @workout_log.routine.day
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,7 +4,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:5173",
     "http://localhost:3000",
+    "http://localhost:5174",
     "https://gym-fitness-frontend-production.up.railway.app",
+    "https://gym-fitness-frontend-tailwind-production.up.railway.app",
     "https://gym-fitness-api-production.up.railway.app"
 
     resource "*",
